0d56f746e2c680572b42df2078c32cb72081a46f
Brandon Mitchell <brandon@systemisdown.net>
2009-04-20 17:04:30 -0700

    Initial stab at a reminder system... I'm coding blind, here. Use at your own risk. ;-)

---
+++ lib/botbckt/commands/remind.rb
@@ -0,0 +1,28 @@
+module Botbckt
+  
+  class Remind
+    extend Commands
+
+    on :remind do |reminder_string, user, *args|
+      _, num, scale, msg = /in (\d+) (\w+) with (.*)/i.match(reminder_string)
+      
+      begin
+        time = num.to_i.send(scale.to_sym).seconds
+        remind(user, msg, time)
+      rescue NoMethodError => e
+        puts "OOPS: #{e.message}"
+        Botbckt::Bot.befuddled
+      end
+    end
+    
+    private
+    
+    def self.remind(user, msg, seconds)
+      EventMachine::Timer.new(seconds) do
+        say "#{user}: #{msg}"
+      end
+    end
+    
+  end
+  
+end
