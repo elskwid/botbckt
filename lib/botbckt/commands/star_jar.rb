e709e8652d39b07a81194062733fb40568474662
Brandon Mitchell <brandon@systemisdown.net>
2009-04-28 22:50:49 -0700

    Add a simple star jar.

---
+++ lib/botbckt/commands/star_jar.rb
@@ -0,0 +1,28 @@
+module Botbckt #:nodoc:
+  
+  # Gives a gold star to a space-separated list of users.
+  #
+  #  < user> ~star joe
+  #  < botbckt> joe: Gold star for you!
+  #
+  # But when a user tries to award himself...
+  #
+  #  < user> ~star user
+  #  < botbckt> user: No star for you!
+  #
+  class StarJar
+    extend Commands
+   
+    on :star do |giver, channel, receiver|
+      receiver.split(' ').each do |rcv|
+        if rcv != giver.gsub(/([^!]+).*/, '\1')
+          say "#{rcv}: Gold star for you!", channel
+        else
+          say "#{rcv}: No star for you!", channel
+        end
+      end
+    end
+    
+  end
+  
+end
