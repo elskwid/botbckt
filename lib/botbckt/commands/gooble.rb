faf4cb8d01f79feb25fbe6f53c2268700528e995
El Skwid (Don Morrison) <elskwid@gmail.com>
2009-04-30 10:29:31 +0800

    Add gooble command for 'lost in translation' action
    Signed-off-by: Brandon Mitchell <brandon@systemisdown.net>

---
+++ lib/botbckt/commands/gooble.rb
@@ -0,0 +1,115 @@
+module Botbckt #:nodoc:
+
+  # Sends text repeatedly to Google Translate via the JSON API 
+  # to garble the output. Translation starts and ends with 
+  # MAIN_LANGUAGE unless a language option is passed in. If
+  # an option is present it is used as the starting language.
+  # 
+  #
+  # < user> ~gooble We the People of the United States, in Order to form a more perfect Union
+  # < botbckt> Popular in the United States to create the completed
+  #
+  # With language option:
+  # 
+  # < user> ~gooble --german Guten tag
+  # < botbckt> Good day
+  #
+  # or the short version
+  #
+  # < user> ~gooble -de Guten tag
+  # < botbckt> Good day
+  
+  class Gooble
+    extend Commands
+  
+    MAIN_LANGUAGE       = "en"
+    TRANSLATE_ATTEMPTS  = 4
+    # Yeah this is wordy but it's self documenting.
+    LANGUAGES           = 
+      {
+        "Albanian"    => "sq",
+        "Arabic"      => "ar",
+        "Bulgarian"   => "bg",
+        "Catalan"     => "ca",
+        "Chinese"     => "zh-CN",
+        "Croatian"    => "hr",
+        "Czech"       => "cs",
+        "Danish"      => "da",
+        "Dutch"       => "nl",
+        "English"     => "en",
+        "Estonian"    => "et",
+        "Filipino"    => "tl",
+        "Finnish"     => "fi",
+        "French"      => "fr",
+        "Galician"    => "gl",
+        "German"      => "de",
+        "Greek"       => "el",
+        "Hebrew"      => "iw",
+        "Hindi"       => "hi",
+        "Hungarian"   => "hu",
+        "Indonesian"  => "id",
+        "Italian"     => "it",
+        "Japanese"    => "ja",
+        "Korean"      => "ko",
+        "Latvian"     => "lv",
+        "Lithuanian"  => "lt",
+        "Maltese"     => "mt",
+        "Norwegian"   => "no",
+        "Polish"      => "pl",
+        "Portuguese"  => "pt",
+        "Romanian"    => "ro",
+        "Russian"     => "ru",
+        "Serbian"     => "sr",
+        "Slovak"      => "sk",
+        "Slovenian"   => "sl",
+        "Spanish"     => "es",
+        "Swedish"     => "sv",
+        "Thai"        => "th",
+        "Turkish"     => "tr",
+        "Ukrainian"   => "uk",
+        "Vietnamese"  => "vi"
+      }
+    
+    on :gooble do |sender, channel, gooble_string|
+      # start with english unless asked for something else
+      gooble_string =~ /(-\w{2}|--\w+)?(.*)/i
+      option, text = $1, $2
+      clean_option = option.gsub(/-/,'') if option
+
+      start_language = case option
+        when /^--/ then
+          LANGUAGES[clean_option.capitalize!] || MAIN_LANGUAGE
+        when /^-/ then
+          LANGUAGES.value?(clean_option) ? clean_option : MAIN_LANGUAGE
+        else
+          MAIN_LANGUAGE
+      end
+
+      # languages to use for the goobling
+      languages = [start_language]
+      languages << LANGUAGES.values.sort_by{ rand }.slice(0...TRANSLATE_ATTEMPTS)
+      languages << MAIN_LANGUAGE # always end with main 
+      
+      result = gooble(text, languages.flatten)
+      say result, channel
+    end
+  
+    private
+
+    # args
+    # text<String>: text to translate
+    # langs<Array>: languages to translate from/to
+    def self.gooble(text, languages) #:nodoc:
+      if languages.length >= 2
+        pair     = "#{languages[0]}|#{languages[1]}"
+        json     = open("http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{CGI.escape(text)}&langpair=#{CGI.escape(pair)}")
+        response = JSON.parse(json.read) # could check for failed response with response['responseStatus'] != 200 
+        languages.shift
+        gooble(response['responseData']['translatedText'], languages)
+      else
+        text
+      end
+    end
+
+  end
+end
