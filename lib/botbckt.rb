7e4921df522b62b278efbbdd478996578b967a78
Brandon Mitchell <brandon@systemisdown.net>
2009-05-03 10:44:17 -0700

    Add hpricot.

--- lib/botbckt.rb
+++ lib/botbckt.rb
@@ -1,4 +1,4 @@
-%w{ rubygems eventmachine activesupport ostruct json open-uri cgi }.each { |lib| require lib }
+%w{ rubygems eventmachine activesupport ostruct json open-uri cgi hpricot }.each { |lib| require lib }
 %w{ irc bot commands }.each { |lib| require File.dirname(__FILE__) + "/botbckt/#{ lib }" }
 
 Botbckt::Bot.start :user => 'botbckt', :server => 'irc.freenode.net', :port => 6667, :channels => ['reno.rb']
