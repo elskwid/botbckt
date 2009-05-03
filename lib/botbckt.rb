2056840c2a58fd81e995eddcfdf734f30ed50501
Brandon Mitchell <brandon@systemisdown.net>
2009-05-03 11:14:49 -0700

    Missing require.

--- lib/botbckt.rb
+++ lib/botbckt.rb
@@ -1,4 +1,4 @@
 %w{ rubygems eventmachine activesupport ostruct json open-uri cgi hpricot }.each { |lib| require lib }
-%w{ irc bot commands }.each { |lib| require File.dirname(__FILE__) + "/botbckt/#{ lib }" }
+%w{ irc bot command commands }.each { |lib| require File.dirname(__FILE__) + "/botbckt/#{ lib }" }
 
 Botbckt::Bot.start :user => 'botbckt', :server => 'irc.freenode.net', :port => 6667, :channels => ['reno.rb']
