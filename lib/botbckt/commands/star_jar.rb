dfe84e563ae05aa26cdc1fc901e9b0d9d4b46d7a
Brandon Mitchell <brandon@systemisdown.net>
2009-05-04 11:21:07 -0700

    The Star Jar should now maintain state.

--- lib/botbckt/commands/star_jar.rb
+++ lib/botbckt/commands/star_jar.rb
@@ -13,11 +13,44 @@ module Botbckt #:nodoc:
   class StarJar < Command
    
     trigger :star
+
+    attr_accessor :jar
+
+    def self.create!(*args) #:nodoc:
+      self.instance.jar ||= { }
+      self.instance
+    end
+   
+    # Adds a star to the jar for the user
+    #
+    # ==== Parameters
+    # user<String>:: The user receiving a star. Required.
+    #
+    def push(user)
+      @jar[user] ||= 0
+      @jar[user] += 1
+    end
+    
+    # Removes a star from the jar for the user
+    #
+    # ==== Parameters
+    # user<String>:: The user being docked a star. Required.
+    #
+    def pop(user)
+      @jar[user] ||= 0
+      
+      if @jar[user] == 0
+        return 0
+      else
+        @jar[user] -= 1
+      end
+    end
    
     def call(giver, channel, receiver)
       receiver.split(' ').each do |rcv|
         if rcv != freenode_split(giver).first
-          say "#{rcv}: Gold star for you!", channel
+          total = push(rcv)
+          say "#{rcv}: Gold star for you! (#{total})", channel
         else
           say "#{rcv}: No star for you!", channel
         end
